f3cfd86148553118e7613b17b7aff096f16249c8
jelena viskovic <jelena.viskovic@gmail.com>
2020-04-25 11:26:46 +0100

    base-runtime-asset-container-added

---
+++ Assets/Scripts/Ayuc-scripts/AyucDataHandler.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AyucDataHandler : MonoBehaviour
+{
+    public GameObject ayucRuntimeAssetParent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ayucRuntimeAssetParent = GameObject.Find("AYUC-runtime-assets");
+
+        if (ayucRuntimeAssetParent != null)
+        {
+            Debug.Log(ayucRuntimeAssetParent + " AYUC runtime parent found!");
+        }
+        else
+        {
+            Debug.LogError(ayucRuntimeAssetParent + " AYUC runtime parent NOT FOUND!");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Ayuc-scripts/AyucNavAgentStore.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class AyucNavAgentStore : ScriptableObject
+{
+    public GameObject[] agentMeshes;
+
+    public Transform[] targets;
+}
