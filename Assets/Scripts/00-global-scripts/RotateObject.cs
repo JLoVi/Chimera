67d2d2206f3a3fa0e07036d4d07d38d7c1b9a8fc
jelena viskovic <jelena.viskovic@gmail.com>
2020-04-30 11:16:45 +0100

    redesign main menu

---
+++ Assets/Scripts/00-global-scripts/RotateObject.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateObject : MonoBehaviour
+{
+
+    public Vector3 rotateDirection;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // gameObject.transform.Rotate(Vector3.left * Time.deltaTime, Space.Self);
+        gameObject.transform.Rotate(rotateDirection * Time.deltaTime*2, Space.World);
+
+
+    }
+}
+
+   
